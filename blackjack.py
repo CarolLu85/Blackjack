5cd9f61e37e0ef1bd1daae5310a2a825200c6351
CarolLu85 <little_shrewdness@hotmail.com>
2023-09-08 14:19:32 +0300

    added more print statement

--- blackjack.py
+++ blackjack.py
@@ -44,6 +44,9 @@ for _ in range(2):
   player_cards(deal_cards(player_cards))
   dealer_cards(deal_cards(dealer_cards)) 
 
+print(f"the player got {player_cards}")
+print(f"the dealer's second card is {dealer_cards[1]}")
+
 while not end_of_game:
   player_score = score(player_cards)
   dealer_score = score(dealer_cards)
@@ -55,6 +58,7 @@ while not end_of_game:
     question = input("type 'hit' to get one more card, type 'stand' to exit \n")
     if question == "hit":
       player_cards.append(deal_cards(player_cards))
+      print(f"now your cards are {player_cards}")
     else:
       print("Game over")
       end_of_game = True
@@ -63,7 +67,10 @@ while not end_of_game:
     dealer_cards.append(deal_cards(dealer_cards))
     dealer_score = sum(dealer_cards)
 
-  print(compare(player_score, dealer_score))
+
+print(f"the dealer got {dealer_cards}") 
+print(f"the player got {player_cards}")
+print(compare(player_score, dealer_score))
 
 
 # unsolved issues: two Ace in a hand. on what condition, change the two Aces to 1, or just simplely change both of them to 1 and move to the next round.
